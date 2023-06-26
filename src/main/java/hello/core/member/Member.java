02d0a1e22f33b6d0e8245431ac4b0ccd54bc74ba
eycha <silvlexonst@gmail.com>
2023-06-26 18:52:40 +0900

    build setting

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+import java.util.PrimitiveIterator;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+}
